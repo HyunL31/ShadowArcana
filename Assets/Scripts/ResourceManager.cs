8bb1ea6ac467b3134d682d98fff7ceabfdd955db
이주현 <juhyun040100@gmail.com>
2026-05-12 17:15:04 +0900

    [feat] UIManager, ResourceManager 및 타이틀 UI 구현

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance;
+
+    private Dictionary<string, AsyncOperationHandle> _handles = new Dictionary<string, AsyncOperationHandle>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LoadAsset<T>(string path, Action<T> callback) where T : UnityEngine.Object
+    {
+        if (_handles.TryGetValue(path, out AsyncOperationHandle handle))
+        {
+            callback?.Invoke(handle.Result as T);
+            return;
+        }
+
+        AsyncOperationHandle<T> loadHandle = Addressables.LoadAssetAsync<T>(path);
+
+        loadHandle.Completed += (op) =>
+        {
+            if (loadHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                _handles[path] = op;
+                callback?.Invoke(op.Result);
+            }
+        };
+    }
+
+    public void InstantiatePrefab(string path, Transform parent, Action<GameObject> callback)
+    {
+        Addressables.InstantiateAsync(path, parent).Completed += (op) =>
+        {
+            callback?.Invoke(op.Result);
+        };
+    }
+}
---
+++ Assets/Scripts/UI/Title.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(CloseTitle);
+    }
+
+    private void CloseTitle()
+    {
+        Debug.Log("봉");
+        UIManager.Instance.CloseUI(UIManager.UIType.Title);
+    }
+}
